2ab6cc649e57d792ba02b9eb7693be21eca4e306
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2022-11-14 20:34:00 +0700

    Рефакторинг

--- src/example/example.cpp
+++ src/example/example.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
+#include <memory>
 
 // https://github.com/alphacep/vosk-api/blob/master/c/test_vosk.c
 
@@ -11,71 +12,31 @@ static constexpr char Voice[] = "D:/Programming/C++/vosk-api/resources/novy_god.
 
 int main(int argc, char const *argv[])
 {
-    setlocale(LC_ALL, "Russian");
+    using ModelPtr = std::unique_ptr<VoskModel, decltype(&vosk_model_free)>;
+    using RecognizerPtr = std::unique_ptr<VoskRecognizer, decltype(&vosk_recognizer_free)>;
 
-    FILE *wavInput{};
-    char buf[3200];
-    int nread{};
-    int final{};
+    ModelPtr model{nullptr, &vosk_model_free};
+    RecognizerPtr recognizer{nullptr, &vosk_recognizer_free};
 
+    model = ModelPtr{vosk_model_new(ModelPath), &vosk_model_free};
+    recognizer = RecognizerPtr{vosk_recognizer_new(model.get(), 44000.0), &vosk_recognizer_free};
+
+    std::string buffer;
     std::string result;
 
-    VoskModel *model{};
-    VoskRecognizer *recognizer{};
-    VoskRecognizer *recognizer2{};
+    std::ifstream in{Voice, std::ios::binary};
+    in.seekg(0, std::ios::end);
+    buffer.resize(in.tellg());
+    in.seekg(44, std::ios::beg);
+    in.read(&buffer[0], buffer.size());
+    in.close();
 
-    model = vosk_model_new(ModelPath);
-    recognizer = vosk_recognizer_new(model, 44000.0);
+    int res = vosk_recognizer_accept_waveform(recognizer.get(), buffer.c_str(), buffer.size());
+    result += vosk_recognizer_final_result(recognizer.get());
+    vosk_recognizer_free(recognizer.get());
 
-    {
-        std::string buffer;
-        std::string result2;
-        recognizer2 = vosk_recognizer_new(model, 44000.0);
-
-        std::ifstream in{Voice, std::ios::binary};
-        in.seekg(0, std::ios::end);
-        buffer.resize(in.tellg());
-        in.seekg(44, std::ios::beg);
-        in.read(&buffer[0], buffer.size());
-        in.close();
-
-        int res = vosk_recognizer_accept_waveform(recognizer2, buffer.c_str(), buffer.size());
-        result2 += vosk_recognizer_final_result(recognizer2);
-        vosk_recognizer_free(recognizer2);
-
-        std::ofstream of2{"output2.txt"};
-        of2 << result2;
-    }
-
-    wavInput = fopen(Voice, "rb");
-    fseek(wavInput, 44, SEEK_SET);
-
-    while (!feof(wavInput))
-    {
-        nread = fread(buf, 1, sizeof(buf), wavInput);
-        final = vosk_recognizer_accept_waveform(recognizer, buf, nread);
-
-        if (final)
-        {
-            result += vosk_recognizer_result(recognizer);
-        }
-        else
-        {
-            result += vosk_recognizer_partial_result(recognizer);
-        }
-    }
-
-    fclose(wavInput);
-
-    result += vosk_recognizer_final_result(recognizer);
-
-    std::cout << result << std::endl;
-
-    std::ofstream of{"output.txt"};
-    of << result;
-
-    vosk_recognizer_free(recognizer);
-    vosk_model_free(model);
+    std::ofstream of2{"output.txt"};
+    of2 << result;
 
     return 0;
 }
